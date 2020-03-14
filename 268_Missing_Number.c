ce2f4e0229abfdef8e75cb358f6191f084a528c6
sjtao <61641368+sjtao@users.noreply.github.com>
2020-03-14 13:49:55 -0400

    Update 268_Missing_Number.c

--- 268_Missing_Number.c
+++ 268_Missing_Number.c
@@ -10,3 +10,17 @@ int missingNumber(int* nums, int numsSize){
     
     return sn;
 }
+
+//bit manipulation
+int missingNumber(int* nums, int numsSize){
+    
+    int i;
+    int sn = numsSize;
+    
+    for (i = 0; i < numsSize; i++)
+    {
+        sn ^= (i ^ nums[i]);
+    }
+    
+    return sn;
+}
