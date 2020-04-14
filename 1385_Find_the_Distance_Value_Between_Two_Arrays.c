dc8497c063f125262901babb1c627a7a7763ca81
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-13 20:43:44 -0400

    Update 1385_Find_the_Distance_Value_Between_Two_Arrays.c

--- 1385_Find_the_Distance_Value_Between_Two_Arrays.c
+++ 1385_Find_the_Distance_Value_Between_Two_Arrays.c
@@ -1,5 +1,5 @@
 
-
+//slow
 int findTheDistanceValue(int* arr1, int arr1Size, int* arr2, int arr2Size, int d){
     
     int dis = 0;
@@ -19,3 +19,26 @@ int findTheDistanceValue(int* arr1, int arr1Size, int* arr2, int arr2Size, int d
     return dis;
 }
 
+// faster than 81.97%
+int findTheDistanceValue(int* arr1, int arr1Size, int* arr2, int arr2Size, int d){
+    
+    int dis = 0;
+    int temp;
+    bool ld;
+    for(int i = 0; i < arr1Size; i++)
+    {
+        ld = 1;
+        for(int j = 0; j < arr2Size; j++)
+        {
+            temp = abs(arr1[i] - arr2[j]);
+            if (temp <= d)
+            {
+                ld = 0;
+                break;
+            }
+        }
+        dis += ld;
+    }
+    
+    return dis;
+}
