a0c96d91f3c5d937f59ac852cd55c1c4f5a239c1
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-13 23:02:10 -0400

    Update 509_Fibonacci_Number.c

--- 509_Fibonacci_Number.c
+++ 509_Fibonacci_Number.c
@@ -17,3 +17,27 @@ int fib(int N){
     
     return result;
 }
+
+/*
+Runtime: 0 ms, faster than 100.00% of C online submissions for Fibonacci Number.
+Memory Usage: 5 MB, less than 100.00% of C online submissions for Fibonacci Number.
+*/
+
+int fib(int N){
+
+    int result[31] = {0};
+    
+    result[1] = 1;
+    
+    if(N <= 1)
+    {
+        return result[N];
+    }
+    
+    for(int i = 2; i < N+1; i++)
+    {
+        result[i] = result[i-1] + result[i-2];
+    }
+    
+    return result[N];
+}
