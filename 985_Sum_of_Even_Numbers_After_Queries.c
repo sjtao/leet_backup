f0ccbdf64d829438efef2056f97835b80af22d0e
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-19 17:07:07 -0400

    Update 985_Sum_of_Even_Numbers_After_Queries.c

--- 985_Sum_of_Even_Numbers_After_Queries.c
+++ 985_Sum_of_Even_Numbers_After_Queries.c
@@ -62,3 +62,59 @@ int* sumEvenAfterQueries(int* A, int ASize, int** queries, int queriesSize, int*
     return qsum;
 }
 
+/*
+Runtime: 152 ms, faster than 94.29% of C online submissions for Sum of Even Numbers After Queries.
+Memory Usage: 21.2 MB, less than 100.00% of C online submissions for Sum of Even Numbers After Queries.
+*/
+
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int evensum(int *a, int size)
+{
+    int i;
+    int sum = 0;
+    
+    for(i = 0; i < size; i++)
+    {
+        if(a[i] % 2 == 0)
+        {
+            sum += a[i];
+        }
+    }
+    
+    return sum;
+}
+
+int* sumEvenAfterQueries(int* A, int ASize, int** queries, int queriesSize, int* queriesColSize, int* returnSize){
+
+    int val, index, i;
+    int *qsum = malloc(queriesSize * sizeof(int));
+    int initevensum = evensum(A, ASize);
+    
+    for(i = 0; i < queriesSize; i++)
+    {
+        val = queries[i][0];
+        index = queries[i][1];
+        
+        if(A[index] % 2 == 0)
+        {
+            initevensum -= A[index];
+        }
+            
+        A[index] += val;
+        
+        if(A[index] % 2 == 0)
+        {
+            initevensum += A[index];
+        }
+        
+        qsum[i] = initevensum; 
+    }
+    
+    *returnSize = queriesSize;
+    
+    return qsum;
+}
