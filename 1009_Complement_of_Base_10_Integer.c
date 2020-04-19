5dad43d4d894f84a4eeea5f1d6557ffb0ac5d4f4
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-18 21:17:10 -0400

    Update 1009_Complement_of_Base_10_Integer.c

--- 1009_Complement_of_Base_10_Integer.c
+++ 1009_Complement_of_Base_10_Integer.c
@@ -23,3 +23,35 @@ int bitwiseComplement(int N){
     return bn;
     
 }
+
+/*
+Runtime: 0 ms, faster than 100.00% of C online submissions for Complement of Base 10 Integer.
+Memory Usage: 5.2 MB, less than 100.00% of C online submissions for Complement of Base 10 Integer.
+*/
+
+int bitwiseComplement(int N){
+    
+    if(N == 0)
+    {
+        return 1;
+    }
+    
+    int binary[32]={0};
+    int i = 0;
+    
+    while(N > 0)
+    {
+        binary[i] = ((N % 2)^1); //0 XOR 1 = 1, 1 XOR 1 = 0
+        N /= 2;
+        i++;
+    }
+    
+    int complement = 0;
+    for(int j = i-1; j >= 0; j--)
+    {
+        complement = complement * 2 + binary[j];
+    }
+    
+    return complement;
+
+}
