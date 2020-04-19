a41835e5f21f31859013c718f037ffc9d34bdb6d
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-18 22:27:36 -0400

    Update 1046_Last_Stone_Weight.c

--- 1046_Last_Stone_Weight.c
+++ 1046_Last_Stone_Weight.c
@@ -3,7 +3,7 @@ Runtime: 4 ms, faster than 19.14% of C online submissions for Last Stone Weight.
 Memory Usage: 5.2 MB, less than 100.00% of C online submissions for Last Stone Weight.
 */
 
-
+//brutle force
 int cmpfunc (const void * a, const void * b) {
    return ( *(int*)a - *(int*)b );
 }
@@ -37,4 +37,36 @@ int lastStoneWeight(int* stones, int stonesSize){
     }
     
     return stones[stonesSize - 1];
+}
+
+
+/*
+Runtime: 4 ms, faster than 19.14% of C online submissions for Last Stone Weight.
+Memory Usage: 5.3 MB, less than 100.00% of C online submissions for Last Stone Weight.
+*/
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+int lastStoneWeight(int* stones, int stonesSize){
+
+    if(stonesSize == 1)
+    {
+        return stones[0];
+    }
+    
+    int x, y;
+    
+    for(int s = stonesSize; s > 1; s--)
+    {
+        qsort(stones, s, sizeof(int), cmpfunc);
+        
+        y = stones[s - 1];
+        x = stones[s - 2];
+        
+        stones[s - 2] = y - x;
+        stones[s - 1] = 0;    
+    }
+    
+    return stones[0];
 }
