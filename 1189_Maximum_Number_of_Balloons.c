070008c734b82ee03ad33abb74d976157608eae5
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-19 23:29:51 -0400

    Update 1189_Maximum_Number_of_Balloons.c

--- 1189_Maximum_Number_of_Balloons.c
+++ 1189_Maximum_Number_of_Balloons.c
@@ -44,3 +44,39 @@ int maxNumberOfBalloons(char * text){
     return (min1 < min21) ? min1 : min21;
 }
 
+
+
+/*
+Runtime: 4 ms, faster than 40.35% of C online submissions for Maximum Number of Balloons.
+Memory Usage: 5.2 MB, less than 100.00% of C online submissions for Maximum Number of Balloons.
+*/
+int maxNumberOfBalloons(char * text){
+
+    int len = strlen(text);
+    int * count = calloc(5, sizeof(int));
+    
+    int i;
+    for(i = 0; i < len; i++)
+    {
+        if(text[i] == 'b') count[0]++;
+        else if(text[i] == 'a') count[1]++;
+        else if(text[i] == 'n') count[2]++;
+        else if(text[i] == 'l') count[3]++;
+        else if(text[i] == 'o') count[4]++;
+    }
+    
+    int min1, min2, min21;
+    min1 = (count[0] < count[1]) ? count[0] : count[1];
+    min1 = (min1 < count[2]) ? min1 : count[2];
+    
+    min2 = (count[3] < count[4]) ? count[3] : count[4];
+    
+    if (min1 < 1 || min2 < 2)
+    {
+        return 0;
+    }
+        
+    min21 = min2/2;
+    
+    return (min1 < min21) ? min1 : min21;
+}
