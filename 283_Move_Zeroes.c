adaf35f2437976c0da34aac142b25b10ddde3a8d
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-21 11:20:02 -0400

    Update 283_Move_Zeroes.c

--- 283_Move_Zeroes.c
+++ 283_Move_Zeroes.c
@@ -3,7 +3,6 @@ Runtime: 20 ms, faster than 29.41% of C online submissions for Move Zeroes.
 Memory Usage: 7.5 MB, less than 100.00% of C online submissions for Move Zeroes.
 */
 
-
 void moveZeroes(int* nums, int numsSize){
 
     int i, j;
@@ -30,3 +29,35 @@ void moveZeroes(int* nums, int numsSize){
     
 }
 
+/*
+Runtime: 12 ms, faster than 86.15% of C online submissions for Move Zeroes.
+Memory Usage: 7.3 MB, less than 100.00% of C online submissions for Move Zeroes.
+*/
+
+void moveZeroes(int* nums, int numsSize){
+
+    if(numsSize < 2)
+    {
+        return;
+    }
+    
+    int i = 0, j = 0;
+    int temp;
+    
+    while(i < numsSize)
+    {
+        if(nums[i] == 0)
+            i++;
+        else
+        {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j++;
+        }
+    }
+    
+    return;
+    
+}
