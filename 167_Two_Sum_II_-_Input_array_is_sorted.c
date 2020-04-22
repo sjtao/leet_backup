ec0a79dd5417d88a7b2fc01f88fda6d4b33a279c
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-21 23:15:57 -0400

    Update 167_Two_Sum_II_-_Input_array_is_sorted.c

--- 167_Two_Sum_II_-_Input_array_is_sorted.c
+++ 167_Two_Sum_II_-_Input_array_is_sorted.c
@@ -31,3 +31,38 @@ int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
     
     return;
 }
+
+/*
+Runtime: 4 ms, faster than 95.31% of C online submissions for Two Sum II - Input array is sorted.
+Memory Usage: 6.5 MB, less than 100.00% of C online submissions for Two Sum II - Input array is sorted.
+*/
+
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
+
+    int *index = malloc(2 * sizeof(int));
+    
+    * returnSize = 2;
+    
+    int l = 0, r = numbersSize-1;
+    bool flag = 1;
+    
+    while(flag && l < r)
+    {
+        if(numbers[l]+numbers[r] < target)
+        {
+            l++;
+        }
+        else if(numbers[l]+numbers[r] > target)
+        {
+            r--;
+        }
+        else
+        {
+            index[0] = l+1;
+            index[1] = r+1;
+            flag = 0;
+        }
+    }
+                
+    return index;
+}
