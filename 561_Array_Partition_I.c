7706d5883a604becc0b08e80c23572906097ecae
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-23 22:57:05 -0400

    Update 561_Array_Partition_I.c

--- 561_Array_Partition_I.c
+++ 561_Array_Partition_I.c
@@ -3,17 +3,8 @@ Runtime: 52 ms, faster than 71.30% of C online submissions for Array Partition I
 Memory Usage: 8.3 MB, less than 100.00% of C online submissions for Array Partition I.
 */
 
-int compare(const void* a, const void* b)
-{
-	const int* x = (int*) a;
-	const int* y = (int*) b;
-
-	if (*x > *y)
-		return 1;
-	else if (*x < *y)
-		return -1;
-
-	return 0;
+int compare(const int* a, const int* b) {
+    return (*a - *b);
 }
 
 int arrayPairSum(int* nums, int numsSize){
