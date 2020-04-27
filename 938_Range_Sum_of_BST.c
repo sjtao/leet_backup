bb27c8bcb3513ef4cdc20e3879d069d2ab850a49
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-27 17:31:37 -0400

    Update 938_Range_Sum_of_BST.c

--- 938_Range_Sum_of_BST.c
+++ 938_Range_Sum_of_BST.c
@@ -29,3 +29,26 @@ int rangeSumBST(struct TreeNode* root, int L, int R){
     return (sum(root->val,L,R) + rangeSumBST(root->right,L,R) +  rangeSumBST(root->left,L,R));
     
 }
+
+/*
+Runtime: 88 ms, faster than 42.01% of C online submissions for Range Sum of BST.
+Memory Usage: 42.2 MB, less than 100.00% of C online submissions for Range Sum of BST.
+*/
+void sum(struct TreeNode* root, int L, int R, int *s){
+    if(root==NULL) return 0;
+    
+    if(L <= root->val && root->val <= R)
+    {
+        *s += root->val;
+    }
+    
+    sum(root->right,L,R,s);
+    sum(root->left,L,R,s);
+}
+
+int rangeSumBST(struct TreeNode* root, int L, int R){
+    if(root==NULL) return 0;
+    int *s=malloc(sizeof(int)); *s = 0;
+    sum(root,L,R,s);
+    return *s;
+}
