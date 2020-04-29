4850991eacb1b961bc63ae89af63389bdcf9e0ca
sjtao <61641368+sjtao@users.noreply.github.com>
2020-04-29 00:04:27 -0400

    Update 492_Construct_the_Rectangle.c

--- 492_Construct_the_Rectangle.c
+++ 492_Construct_the_Rectangle.c
@@ -19,3 +19,25 @@ int* constructRectangle(int area, int* returnSize){
     *returnSize = 2;
     return rectangle;
 }
+
+/*
+Runtime: 0 ms, faster than 100.00% of C online submissions for Construct the Rectangle.
+Memory Usage: 5.2 MB, less than 100.00% of C online submissions for Construct the Rectangle.
+*/
+int* constructRectangle(int area, int* returnSize){
+    
+    int *rectangle = calloc(2, sizeof(int));
+    
+    int x = sqrt(area);
+    
+    while(area % x != 0)
+    {
+        x--;
+    }
+    
+    rectangle[0] = area / x;
+    rectangle[1] = x;
+    
+    *returnSize = 2;
+    return rectangle;
+}
