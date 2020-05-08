8743000f97f667506ae4ba2ea672d387da9a1b79
sjtao <61641368+sjtao@users.noreply.github.com>
2020-05-07 23:41:19 -0400

    Update 543_Diameter_of_Binary_Tree.c

--- 543_Diameter_of_Binary_Tree.c
+++ 543_Diameter_of_Binary_Tree.c
@@ -26,3 +26,29 @@ int diameterOfBinaryTree(struct TreeNode* root){
     
     return max > nodeDiameter ? max : nodeDiameter;
 }
+
+/*
+Runtime: 8 ms, faster than 71.34% of C online submissions for Diameter of Binary Tree.
+Memory Usage: 7.7 MB, less than 100.00% of C online submissions for Diameter of Binary Tree.
+*/
+
+int maxDepth(struct TreeNode* root, int *max){
+    
+    if(!root) return 0;
+    
+    int r=maxDepth(root->right, max);
+    int l=maxDepth(root->left, max);
+    
+    *max = (r+l) > *max ? (r+l) : *max;
+    return (r>l?r:l) + 1;
+}
+
+
+int diameterOfBinaryTree(struct TreeNode* root){
+
+    if(!root) return 0;
+    
+    int max = 0;
+    maxDepth(root, &max);
+    return max;
+}
