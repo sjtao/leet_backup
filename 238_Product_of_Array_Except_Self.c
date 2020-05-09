7a039ae4672406ab7343211354359c7c23ea2139
sjtao <61641368+sjtao@users.noreply.github.com>
2020-05-09 13:51:12 -0400

    Create 238_Product_of_Array_Except_Self.c

---
+++ 238_Product_of_Array_Except_Self.c
@@ -0,0 +1,40 @@
+/*
+Runtime: 44 ms, faster than 94.25% of C online submissions for Product of Array Except Self.
+Memory Usage: 14 MB, less than 100.00% of C online submissions for Product of Array Except Self.
+*/
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+int* productExceptSelf(int* nums, int numsSize, int* returnSize){
+    
+    *returnSize = numsSize;
+    
+    if(numsSize < 2)
+    {
+        return nums;
+    }
+    
+    int *prod = malloc(numsSize * sizeof(int));
+    int *left = malloc(numsSize * sizeof(int));
+    int *right = malloc(numsSize * sizeof(int));
+    
+    left[0] = 1;
+    right[numsSize-1] = 1;
+    
+    int i;
+    for(i = 1; i < numsSize; i++)
+    {
+        left[i] = left[i-1] * nums[i-1];
+        right[numsSize-1-i] = right[numsSize-i] * nums[numsSize-i];
+    }
+    
+    for(i=0; i<numsSize; i++)
+    {
+        prod[i] = left[i]*right[i];
+    }
+    
+    return prod;
+    
+}
