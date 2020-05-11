b0d32dd5c31269d9004801ee3648ce8cda7e0ed5
sjtao <61641368+sjtao@users.noreply.github.com>
2020-05-10 22:41:38 -0400

    Update 762_Prime_Number_of_Set_Bits_in_Binary_Representation.c

--- 762_Prime_Number_of_Set_Bits_in_Binary_Representation.c
+++ 762_Prime_Number_of_Set_Bits_in_Binary_Representation.c
@@ -48,3 +48,50 @@ int countPrimeSetBits(int L, int R){
     
     return primecount;
 }
+
+/*
+Runtime: 4 ms, faster than 100.00% of C online submissions for Prime Number of Set Bits in Binary Representation.
+Memory Usage: 5 MB, less than 100.00% of C online submissions for Prime Number of Set Bits in Binary Representation.
+*/
+int countone(int m)
+{
+    int count = 0;
+    
+    while(m != 0)
+    {
+        m = m&(m-1);
+        count++;
+    }
+    
+    return count;
+}
+
+bool prime(int m)
+{
+    if(m < 2)
+    {
+        return 0;
+    }
+    
+    for(int i = 2; i*i<= m; i++)
+    {
+        if (m%i == 0)
+        {
+            return 0;
+        }
+    }
+    
+    return 1;
+}
+
+int countPrimeSetBits(int L, int R){
+    
+    int primecount = 0;
+    
+    for(int i = L; i <= R; i++)
+    {
+        primecount += prime(countone(i));
+    }
+    
+    return primecount;
+}
