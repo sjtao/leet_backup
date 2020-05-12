59d7ebcb5e3ec6eac3fa56a00ff3de2f90de407e
sjtao <61641368+sjtao@users.noreply.github.com>
2020-05-11 22:04:31 -0400

    Create 1431_Kids_With_the_Greatest_Number_of_Candies.c

---
+++ 1431_Kids_With_the_Greatest_Number_of_Candies.c
@@ -0,0 +1,27 @@
+/*
+Runtime: 4 ms, faster than 79.43% of C online submissions for Kids With the Greatest Number of Candies.
+Memory Usage: 5.8 MB, less than 100.00% of C online submissions for Kids With the Greatest Number of Candies.
+*/
+
+bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize){
+
+    int max = candies[0];
+    int i;
+    for(i = 1; i < candiesSize; i++)
+    {
+        max = (max > candies[i] ? max : candies[i]);
+    }
+    
+    bool *great = malloc(candiesSize * sizeof(bool));
+    int em = max - extraCandies;
+    
+    for(i = 0; i < candiesSize; i++)
+    {
+        great[i] = (candies[i] >= em ? 1 : 0);
+    }
+    
+    *returnSize = candiesSize;
+    
+    return great;
+    
+}
