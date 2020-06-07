69c51039e283341f3afae452420bb02788c8247d
sjtao <61641368+sjtao@users.noreply.github.com>
2020-06-06 23:57:42 -0400

    Update 121_Best_Time_to_Buy_and_Sell_Stock.c

--- 121_Best_Time_to_Buy_and_Sell_Stock.c
+++ 121_Best_Time_to_Buy_and_Sell_Stock.c
@@ -27,3 +27,33 @@ int maxProfit(int* prices, int pricesSize){
     
     return df;
 }
+
+/*
+Runtime: 8 ms, faster than 42.52% of C online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 6.5 MB, less than 49.32% of C online submissions for Best Time to Buy and Sell Stock.
+*/
+int maxProfit(int* prices, int pricesSize){
+    
+    if(pricesSize < 2)
+    {
+        return 0;
+    }
+    
+    int i, j, k;
+    int min = prices[0];
+    int max = 0;
+    
+    for(i = 0; i < pricesSize; i++)
+    {
+        if(prices[i] < min)
+        {
+            min = prices[i];
+        }
+        else if(prices[i] - min > max)
+        {
+            max = prices[i] - min;
+        }
+    }
+    
+    return max;
+}
