7799dfdee258cac9b83789da6b17a1c6888bbf68
sjtao <61641368+sjtao@users.noreply.github.com>
2020-07-18 15:45:33 -0400

    Update 189_Rotate_Array.c

--- 189_Rotate_Array.c
+++ 189_Rotate_Array.c
@@ -31,3 +31,70 @@ void rotate(int* nums, int numsSize, int k){
     
     return;
 }
+
+/*
+Runtime: 640 ms, faster than 5.78% of C online submissions for Rotate Array.
+Memory Usage: 7.2 MB, less than 89.74% of C online submissions for Rotate Array.
+*/
+void swap(int c, int numsSize, int* nums)
+{
+    int i, temp;
+    temp = nums[numsSize-1];
+    for(i = numsSize - 1; i > 0; i--)
+    {
+        nums[i] = nums[i-1];
+    }
+    nums[i] = temp;
+    
+    return;
+}
+
+void rotate(int* nums, int numsSize, int k){
+    
+    if( k % numsSize == 0)
+    {
+        return;
+    }
+    
+    k = k % numsSize;
+    int c = 0;
+    while (c < k)
+    {
+        swap(c, numsSize, nums);
+        c++;
+    }
+    
+    return;
+}
+
+/*
+Runtime: 8 ms, faster than 99.22% of C online submissions for Rotate Array.
+Memory Usage: 7.5 MB, less than 13.46% of C online submissions for Rotate Array.
+*/
+void rotate(int* nums, int numsSize, int k){
+    
+    if( k % numsSize == 0)
+    {
+        return;
+    }
+    
+    k = k % numsSize;
+    int current, next, prev, temp;
+    int i, count = 0;
+    for(i = 0; count < numsSize; i ++)
+    {
+        prev = nums[i];
+        current = i;
+        do
+        {
+            next = (current + k) % numsSize;
+            temp = nums[next];
+            nums[next] = prev;
+            prev = temp;
+            current = next;
+            count ++;
+        }while(i!=current);
+    }
+ 
+    return;
+}
